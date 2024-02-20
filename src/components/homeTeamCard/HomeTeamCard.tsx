475d9342f8799105272c3c86502415485c84ceb9
ArielCorrado <jarielc2012@yahoo.com.ar>
2024-02-20 19:02:55 -0300

    se agrego seccion team

--- src/components/homeTeamCard/HomeTeamCard.tsx
+++ src/components/homeTeamCard/HomeTeamCard.tsx
@@ -7,6 +7,7 @@ export default function HomeTeamCard (props: {imgPath: string, mainText: string,
             <div className="homeConcernsCard_textCont flex column">
                 <p className="homeConcernsCard_mainText">{props.mainText}</p>
                 <p className="homeConcernsCard_secondaryText">{props.secondaryText}</p>
+                <p className="homeConcernsCard_mainText homeTeamCard_viewCVText flex"><span className="homeTeamCard_viewCVText_Span">VER CURRICULUM</span></p>
             </div>
         </div>
     );
