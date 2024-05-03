7e13b67f7d3388583d61ac0900a52e7f99c7c133
ArielCorrado <jarielc2012@yahoo.com.ar>
2024-05-03 20:00:46 -0300

    cambios 3/5

--- src/utils/swal.ts
+++ src/utils/swal.ts
@@ -9,14 +9,16 @@ const swalPopUp = (title: string, text: string, icon: SweetAlertIcon) => {
         confirmButtonColor: "#3E5C76",
         iconColor: "#3E5C76",
         color: "#384153",
-        scrollbarPadding: false,
+        scrollbarPadding: true,
         customClass: {
             confirmButton: "sweetConfirmBoton",
         },
     };
 
+    const navBar = document.querySelector(".navBar_navBarInfo_Cont") as HTMLDivElement;         //Para que el navbar ocupe el ancho del viewsreen menos el ancho del scroll
+    navBar.style.width = document.body.offsetWidth + "px";
+           
     Swal.fire(options);
-
 };
 
 export {swalPopUp};
